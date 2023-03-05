0e181949f5a0eb05974a211d7b021d0697960a6a
Aiman <aimananjum2001@gmail.com>
2023-03-05 23:04:08 +0530

    modified

--- index.js
+++ index.js
@@ -22,3 +22,35 @@ function updateCounter(){
  totalCounterEl.innerText = textareaEL.value.length
  remainingCounterEl.innerText = textareaEL.getAttribute("maxLength")-textareaEL.value.length;
 }
+
+const containerEl = document.querySelector(".c-container");
+
+for(let i=0;i<30;i++){
+    const colorContainerEl = document.createElement("div");
+    colorContainerEl.classList.add("color-container");
+    containerEl.appendChild(colorContainerEl);
+}
+
+const colorContainerEls = document.querySelectorAll(".color-container");
+
+generateColor()
+
+function generateColor(){
+    colorContainerEls.forEach((colorContainerEl)=>{
+        const newColorCode = randomColor()
+        colorContainerEl.style.backgroundColor="#"+newColorCode;
+        colorContainerEl.innerText = "#"+newColorCode;
+    });
+}
+
+function randomColor(){
+    const chars = "0123456789abcdef"
+    const colorCodeLength = 6;
+    let colorCode = "";
+    for(let ind=0;ind<colorCodeLength;ind++){
+        const randomNum = Math.floor(Math.random()*chars.length);
+        colorCode +=chars.substring(randomNum,randomNum+1);
+        
+    }
+    return colorCode;
+}
